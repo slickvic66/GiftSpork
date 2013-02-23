7c742ad2a2695de7c779d6013bde7564f24c2263
Victor Oleinikov <victor.oleinikov@gmail.com>
2013-02-22 21:10:58 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftr::Application.config.secret_token = '1bb2cf4885c1107f13b68a3565869e3de0c94ee58675def4efe94206dd2fa91311dc67df5d400f902c9b5dbf60397f81b79acdefa845a92ee27ac4a85e3b5a21'
