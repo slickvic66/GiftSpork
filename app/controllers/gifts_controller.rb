e734afc59a105305d7182da0cbf7a36f6dd57ebd
Victor Oleinikov <victor.oleinikov@gmail.com>
2013-03-19 15:24:27 -0700

    Removed loading of gifts in gift index

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -3,24 +3,9 @@ class GiftsController < ApplicationController
 
   def index
     @exchange = Exchange.find(params[:exchange_id])
-    @gifts = @exchange.gifts_below_max_price
     @current_gift = current_user.gift_on_current_exchange(@exchange).first
     @gift_ideas = current_user.gift_ideas_for_exchange(@exchange)
     @etsy_gifts = get_all_listings_for_price(@exchange.max_price/100)["results"]
-  end
-
-  # Pretty hacky.  Meant to be done with AJAX
-  def add_gift
-    match = Match.find_by_sql(["SELECT matches.* FROM matches WHERE matches.santa_id = :current_user_id AND matches.exchange_id = :this_exchange_id", :current_user_id => current_user.id, 
-                          :this_exchange_id => params[:exchange_id]]).first
-
-    if match.update_attributes(gift_id: params[:id])
-      flash[:success] = "Gift Selected"
-      redirect_to exchange_path(params[:exchange_id])
-    else
-      flash[:error] = "Gift Not Selected"
-      redirect_to exchange_gifts_path(params[:exchange_id])
-    end
   end
 
 private
